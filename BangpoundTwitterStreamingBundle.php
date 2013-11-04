2371a595538c1b2aa153fc03297cb4c7f9b9afcf
Benjamin Doherty <bjd@bangpound.org>
2013-11-04 02:39:59 -0600

    merge twitter miner bundle.

--- BangpoundTwitterStreamingBundle.php
+++ BangpoundTwitterStreamingBundle.php
@@ -6,4 +6,10 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BangpoundTwitterStreamingBundle extends Bundle
 {
+    public function build(\Symfony\Component\DependencyInjection\ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new NotificationAdminCompilerPass());
+    }
 }
---
+++ DependencyInjection/Compiler/NotificationAdminCompilerPass.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bangpound\Bundle\TwitterStreamingBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+class NotificationAdminCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition('sonata.notification.admin.message')) {
+            return;
+        }
+
+        $container->removeDefinition('sonata.notification.admin.message');
+    }
+}
